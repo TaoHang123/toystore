5dc11b237b4657e9891bb94bad42ab72b05596cc
TaoHang <tao_hang@163.com>
2026-06-18 16:51:51 +0800

    上线前提交

--- toystore/settings.py
+++ toystore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'HangTao.pythonanywhere.com']
 
 
 # Application definition
